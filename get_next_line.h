ca4d62f54954acb277d6d166f15cd88eea2da540
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-08-14 18:35:11 -0700

    stupid

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/13 15:58:28 by olkovale          #+#    #+#             */
-/*   Updated: 2017/08/13 15:58:28 by olkovale         ###   ########.fr       */
+/*   Updated: 2017/08/14 18:31:45 by olkovale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE (4)
+# define BUFF_SIZE (256)
 # define FD_MAX (4864 + 1)
 
 int		get_next_line(const int fd, char **line);
