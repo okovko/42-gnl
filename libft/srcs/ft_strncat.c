7306d9910ec8371cdaee0e1287a016800cf808c6
okovko <okovko@gmail.com>
2017-09-11 16:48:38 -0500

    fixed some str functions

--- libft/srcs/ft_strncat.c
+++ libft/srcs/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 06:37:18 by olkovale          #+#    #+#             */
-/*   Updated: 2017/09/06 15:13:10 by olkovale         ###   ########.fr       */
+/*   Updated: 2017/09/11 16:39:54 by oleg             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 
 char	*ft_strncat(char *dst, const char *src, int sz)
 {
+	char	cc;
 	char	*beg;
 
 	beg = dst;
 	while (*dst)
 		dst++;
-	while (*src && sz--)
-		*dst++ = *src++;
+	while ((cc = *src++) && sz--)
+		*dst++ = cc;
 	*dst = '\0';
 	return (beg);
 }
--- libft/srcs/ft_strncpy.c
+++ libft/srcs/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/07 11:29:19 by olkovale          #+#    #+#             */
-/*   Updated: 2017/09/06 15:09:57 by olkovale         ###   ########.fr       */
+/*   Updated: 2017/09/11 16:44:46 by oleg             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 
 char	*ft_strncpy(char *dst, const char *src, int sz)
 {
+	char	cc;
 	char	*beg;
 
 	beg = dst;
-	while (*src && sz--)
-		*dst++ = *src++;
+	while ((cc = *src++) && sz--)
+		*dst++ = cc;
 	while (sz--)
 		*dst++ = '\0';
 	return (beg);
